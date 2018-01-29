23654cf81b77ac9ef8684aec008be985102f1088
fiorezhang <fiorezhang@sina.com>
2018-01-29 15:53:32 +0800

    Add files via upload

---
+++ dcgan_debug.py
@@ -0,0 +1,235 @@
+'''
+D: 
+_________________________________________________________________
+Layer (type)                 Output Shape              Param #
+=================================================================
+conv2d_1 (Conv2D)            (None, 28, 28, 64)        1664
+_________________________________________________________________
+activation_1 (Activation)    (None, 28, 28, 64)        0
+_________________________________________________________________
+max_pooling2d_1 (MaxPooling2 (None, 14, 14, 64)        0
+_________________________________________________________________
+conv2d_2 (Conv2D)            (None, 10, 10, 128)       204928
+_________________________________________________________________
+activation_2 (Activation)    (None, 10, 10, 128)       0
+_________________________________________________________________
+max_pooling2d_2 (MaxPooling2 (None, 5, 5, 128)         0
+_________________________________________________________________
+flatten_1 (Flatten)          (None, 3200)              0
+_________________________________________________________________
+dense_1 (Dense)              (None, 1024)              3277824
+_________________________________________________________________
+activation_3 (Activation)    (None, 1024)              0
+_________________________________________________________________
+dense_2 (Dense)              (None, 1)                 1025
+_________________________________________________________________
+activation_4 (Activation)    (None, 1)                 0
+=================================================================
+
+G: 
+_________________________________________________________________
+Layer (type)                 Output Shape              Param #
+=================================================================
+dense_3 (Dense)              (None, 1024)              103424
+_________________________________________________________________
+activation_5 (Activation)    (None, 1024)              0
+_________________________________________________________________
+dense_4 (Dense)              (None, 6272)              6428800
+_________________________________________________________________
+batch_normalization_1 (Batch (None, 6272)              25088
+_________________________________________________________________
+activation_6 (Activation)    (None, 6272)              0
+_________________________________________________________________
+reshape_1 (Reshape)          (None, 7, 7, 128)         0
+_________________________________________________________________
+up_sampling2d_1 (UpSampling2 (None, 14, 14, 128)       0
+_________________________________________________________________
+conv2d_3 (Conv2D)            (None, 14, 14, 64)        204864
+_________________________________________________________________
+activation_7 (Activation)    (None, 14, 14, 64)        0
+_________________________________________________________________
+up_sampling2d_2 (UpSampling2 (None, 28, 28, 64)        0
+_________________________________________________________________
+conv2d_4 (Conv2D)            (None, 28, 28, 1)         1601
+_________________________________________________________________
+activation_8 (Activation)    (None, 28, 28, 1)         0
+=================================================================
+'''
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Reshape
+from keras.layers.core import Activation
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import UpSampling2D
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.layers.core import Flatten
+from keras.optimizers import SGD
+from keras.datasets import mnist
+import numpy as np
+from PIL import Image
+import argparse
+import math
+
+
+def generator_model():
+    model = Sequential()
+    model.add(Dense(input_dim=100, output_dim=1024))
+    model.add(Activation('tanh'))
+    model.add(Dense(128*7*7))
+    model.add(BatchNormalization())
+    model.add(Activation('tanh'))
+    model.add(Reshape((7, 7, 128), input_shape=(128*7*7,)))
+    model.add(UpSampling2D(size=(2, 2)))
+    model.add(Conv2D(64, (5, 5), padding='same'))
+    model.add(Activation('tanh'))
+    model.add(UpSampling2D(size=(2, 2)))
+    model.add(Conv2D(1, (5, 5), padding='same'))
+    model.add(Activation('tanh'))
+    model.summary()
+    return model
+
+
+def discriminator_model():
+    model = Sequential()
+    model.add(
+            Conv2D(64, (5, 5),
+            padding='same',
+            input_shape=(28, 28, 1))
+            )
+    model.add(Activation('tanh'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(128, (5, 5)))
+    model.add(Activation('tanh'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(1024))
+    model.add(Activation('tanh'))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+    model.summary()
+    return model
+
+
+def generator_containing_discriminator(g, d):
+    model = Sequential()
+    model.add(g)
+    d.trainable = False
+    model.add(d)
+    return model
+
+
+def combine_images(generated_images):
+    num = generated_images.shape[0]
+    width = int(math.sqrt(num))
+    height = int(math.ceil(float(num)/width))
+    shape = generated_images.shape[1:3]
+    image = np.zeros((height*shape[0], width*shape[1]),
+                     dtype=generated_images.dtype)
+    for index, img in enumerate(generated_images):
+        i = int(index/width)
+        j = index % width
+        image[i*shape[0]:(i+1)*shape[0], j*shape[1]:(j+1)*shape[1]] = \
+            img[:, :, 0]
+    return image
+
+
+def train(BATCH_SIZE):
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+    X_train = (X_train.astype(np.float32) - 127.5)/127.5
+    X_train = X_train[:, :, :, None]
+    X_test = X_test[:, :, :, None]
+    # X_train = X_train.reshape((X_train.shape, 1) + X_train.shape[1:])
+    d = discriminator_model()
+    g = generator_model()
+    d_on_g = generator_containing_discriminator(g, d)
+    d_optim = SGD(lr=0.0005, momentum=0.9, nesterov=True)
+    g_optim = SGD(lr=0.0005, momentum=0.9, nesterov=True)
+    g.compile(loss='binary_crossentropy', optimizer="SGD")
+    d_on_g.compile(loss='binary_crossentropy', optimizer=g_optim)
+    d.trainable = True
+    d.compile(loss='binary_crossentropy', optimizer=d_optim)
+    d_loss_best = g_loss_best = 1
+    for epoch in range(100):
+        print("Epoch is", epoch)
+        print("Number of batches", int(X_train.shape[0]/BATCH_SIZE))
+        for index in range(int(X_train.shape[0]/BATCH_SIZE)):
+            noise = np.random.uniform(-1, 1, size=(BATCH_SIZE, 100))
+            image_batch = X_train[index*BATCH_SIZE:(index+1)*BATCH_SIZE]
+            generated_images = g.predict(noise, verbose=0)
+            if index % 20 == 0:
+                image = combine_images(generated_images)
+                image = image*127.5+127.5
+                Image.fromarray(image.astype(np.uint8)).save(
+                    str(epoch)+"_"+str(index)+".png")
+            X = np.concatenate((image_batch, generated_images))
+            y = [1] * BATCH_SIZE + [0] * BATCH_SIZE
+            d_loss = d.train_on_batch(X, y)
+            print("batch %d d_loss : %f" % (index, d_loss))
+            noise = np.random.uniform(-1, 1, (BATCH_SIZE, 100))
+            d.trainable = False
+            g_loss = d_on_g.train_on_batch(noise, [1] * BATCH_SIZE)
+            d.trainable = True
+            print("batch %d g_loss : %f" % (index, g_loss))
+            if d_loss<d_loss_best and g_loss<g_loss_best:
+                d_loss_best, g_loss_best = d_loss, g_loss
+                g.save_weights('generator', True)
+                d.save_weights('discriminator', True)
+                print("Overwrite")
+
+
+def generate(BATCH_SIZE, nice=False, rand=False):
+    g = generator_model()
+    g.compile(loss='binary_crossentropy', optimizer="SGD")
+    g.load_weights('generator')
+    if nice:
+        d = discriminator_model()
+        d.compile(loss='binary_crossentropy', optimizer="SGD")
+        d.load_weights('discriminator')
+        noise = np.random.uniform(-1, 1, (BATCH_SIZE*20, 100))
+        generated_images = g.predict(noise, verbose=1)
+        d_pret = d.predict(generated_images, verbose=1)
+        index = np.arange(0, BATCH_SIZE*20)
+        index.resize((BATCH_SIZE*20, 1))
+        pre_with_index = list(np.append(d_pret, index, axis=1))
+        pre_with_index.sort(key=lambda x: x[0], reverse=True)
+        nice_images = np.zeros((BATCH_SIZE,) + generated_images.shape[1:3], dtype=np.float32)
+        nice_images = nice_images[:, :, :, None]
+        for i in range(BATCH_SIZE):
+            idx = int(pre_with_index[i][1])
+            nice_images[i, :, :, 0] = generated_images[idx, :, :, 0]
+        image = combine_images(nice_images)
+    elif rand:
+        noise = np.random.uniform(0, 0, (BATCH_SIZE, 100))
+        for j in range(BATCH_SIZE): 
+            noise_seed = (2*j-BATCH_SIZE)/BATCH_SIZE
+            noise[j] = np.random.uniform(noise_seed, noise_seed, (100, ))
+        #print(noise)
+        generated_images = g.predict(noise, verbose=1)
+        image = combine_images(generated_images)
+    else:
+        noise = np.random.uniform(-1, 1, (BATCH_SIZE, 100))
+        generated_images = g.predict(noise, verbose=1)
+        image = combine_images(generated_images)
+    image = image*127.5+127.5
+    Image.fromarray(image.astype(np.uint8)).save(
+        "generated_image.png")
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", type=str)
+    parser.add_argument("--batch_size", type=int, default=128)
+    parser.add_argument("--nice", dest="nice", action="store_true")
+    parser.add_argument("--rand", dest="rand", action="store_true")
+    parser.set_defaults(nice=False)
+    parser.set_defaults(rand=False)
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = get_args()
+    if args.mode == "train":
+        train(BATCH_SIZE=args.batch_size)
+    elif args.mode == "generate":
+        generate(BATCH_SIZE=args.batch_size, nice=args.nice, rand=args.rand)
